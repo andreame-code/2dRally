45334b3312045ce1bb9413f888d316ee938f1c3e
andreame-code <andrea.pasqualini.me@gmail.com>
2025-08-26 00:53:44 +0200

    Implement basic track rendering

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pygame
 from game.physics import CarPhysics
 from game.camera import Camera
 from game.input_handler import get_input
-from game.track_loader import load_random_track
+from game.track_loader import load_random_track, parse_track
 
 
 def init_pygame():
@@ -32,7 +32,8 @@ class Game:
         self.running = True
         self.physics = CarPhysics()
         self.camera = Camera(offset=(400, 300))
-        self.track = load_random_track()
+        track_path = load_random_track()
+        self.track = parse_track(track_path) if track_path else []
 
     def run(self):
         while self.running:
@@ -46,6 +47,11 @@ class Game:
             self.physics.update(throttle, brake, steering, dt)
             self.camera.update(self.physics.position)
             self.screen.fill((0, 0, 0))
+
+            for rect in self.track:
+                draw_rect = rect.move(-self.camera.position[0], -self.camera.position[1])
+                pygame.draw.rect(self.screen, (100, 100, 100), draw_rect)
+
             car_rect = pygame.Rect(0, 0, 40, 20)
             draw_pos = (
                 int(self.physics.position.x - self.camera.position[0]),
